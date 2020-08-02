6582df8ab67dc3a36e61e01c8bf8b82c68ed0b5c
Sviatoslav Andrushko <andsviat@gmail.com>
2020-08-02 12:38:25 +0300

    TESTING: Add Webpack configuration for testing and Mocha HTML for reporting

---
+++ config/webpack.test.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './test/mocha.config.js',
+    output: {
+        path: path.resolve(__dirname, 'test'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.spec\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [
+                            ['@babel/plugin-proposal-class-properties']
+                        ]
+                    }
+                }]
+            },
+            {
+                test: /.(css|sass|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /.html$/,
+                use: ['html-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'TodoList Mocha Tests',
+            template: path.resolve('test', 'index.spec.html')
+        })
+    ]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,16 @@
-const config = require('./config/webpack.dev');
+const devConfig = require('./config/webpack.dev');
+const testConfig = require('./config/webpack.test');
+const prodConfig = require('./config/webpack.prod');
+const env = process.env.NODE_ENV;
 
-module.exports = config;
+if (env === 'development') {
+    module.exports = devConfig;
+}
+
+if (env === 'testing') {
+    module.exports = testConfig;
+}
+
+if (env === 'production') {
+    module.exports = prodConfig;
+};
